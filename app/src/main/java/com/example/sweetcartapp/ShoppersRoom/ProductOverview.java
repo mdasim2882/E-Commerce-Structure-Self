57b191a0b6dc087730377655fe57670663850503
mdasim2882 <mohdasim2882@gmail.com>
2020-11-11 19:08:50 +0530

    ProductOverview Activity now only requires invoice calculation, image allocation and button operations.

--- app/src/main/java/com/example/sweetcartapp/ShoppersRoom/ProductOverview.java
+++ app/src/main/java/com/example/sweetcartapp/ShoppersRoom/ProductOverview.java
@@ -1,6 +1,11 @@
 package com.example.sweetcartapp.ShoppersRoom;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +13,41 @@ import com.example.sweetcartapp.R;
 
 public class ProductOverview extends AppCompatActivity {
 
+    TextView qtyamount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_overview);
+        qtyamount = findViewById(R.id.qtyTextChange);
+        setSpinner();
+
+    }
+
+    private void setSpinner() {
+        Spinner spinner = findViewById(R.id.qtySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        //Set item selected listener on spinner dropdown
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String s = "" + adapter.getItem(position);
+                if (!s.equals("Set Quantity")) {
+                    qtyamount.setText(s);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 }
